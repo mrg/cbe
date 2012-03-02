1ad82e8ef7ca83415771ddec91319cd4fd257fe6
mrg <blacknext@gmail.com>
2012-03-01 19:43:43 -0500

    Renamed BasicInserts to BasicInserts1 and split examples to add BasicInserts2 and BasicInserts3

---
+++ InsertingObjects/BasicInserts2/src/main/java/cbe/inserting/utilities/FileLoader.java
@@ -0,0 +1,59 @@
+package cbe.inserting.utilities;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class to load a file via the classpath.
+ *
+ * @author mrg
+ */
+public class FileLoader
+{
+    private static FileLoader instance = null;
+
+    private FileLoader() { }
+
+    public static FileLoader getInstance()
+    {
+        if (instance == null)
+            instance = new FileLoader();
+
+        return instance;
+    }
+
+    public List<String> loadLines(String filename)
+    {
+        BufferedReader    bufferedReader    = null;
+        InputStream       inputStream       = null;
+        InputStreamReader inputStreamReader = null;
+        List<String>      lines             = new ArrayList<String>();
+
+        try
+        {
+            inputStream       = getClass().getClassLoader().getResourceAsStream(filename);
+            inputStreamReader = new InputStreamReader(inputStream);
+            bufferedReader    = new BufferedReader(inputStreamReader);
+
+            String line = null;
+
+            while ((line = bufferedReader.readLine()) != null)
+                lines.add(line);
+
+            inputStream.close();
+            inputStreamReader.close();
+            bufferedReader.close();
+        }
+        catch (IOException e)
+        {
+            System.err.println("Failed to read and/or close file:");
+            e.printStackTrace();
+        }
+
+        return lines;
+    }
+}
---
+++ InsertingObjects/BasicInserts2/src/main/java/cbe/inserting/utilities/Populator.java
@@ -0,0 +1,36 @@
+package cbe.inserting.utilities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utility class to help populate the database with data.
+ *
+ * @author mrg
+ */
+public class Populator
+{
+	// Constants for index positions into data.
+	public static int PERSON_FIRST_NAME = 0;
+	public static int PERSON_LAST_NAME  = 1;
+	
+    private static FileLoader   fileLoader = FileLoader.getInstance();
+    private static List<String[]> people = null;
+
+
+    /**
+     * @return All the first names from src/main/resources/People.txt.
+     */
+    public static List<String[]> getPeople()
+    {
+        if (people == null)
+        {
+        	people = new ArrayList<String[]>();
+        	
+        	for (String line : fileLoader.loadLines("People.txt"))
+        		people.add(line.split("\\|"));
+        }
+        
+        return people;
+    }
+}
