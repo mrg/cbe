33f83db7ea483f38cc3f2df5722b2302f1dac204
mrg <blacknext@gmail.com>
2012-03-01 21:13:36 -0500

    Added email address constant.

--- InsertingObjects/BasicInserts2/src/main/java/cbe/inserting/utilities/Populator.java
+++ InsertingObjects/BasicInserts2/src/main/java/cbe/inserting/utilities/Populator.java
@@ -11,8 +11,9 @@ import java.util.List;
 public class Populator
 {
 	// Constants for index positions into data.
-	public static int PERSON_FIRST_NAME = 0;
-	public static int PERSON_LAST_NAME  = 1;
+	public static int PERSON_FIRST_NAME    = 0;
+	public static int PERSON_LAST_NAME     = 1;
+	public static int PERSON_EMAIL_ADDRESS = 1;
 	
     private static FileLoader   fileLoader = FileLoader.getInstance();
     private static List<String[]> people = null;
