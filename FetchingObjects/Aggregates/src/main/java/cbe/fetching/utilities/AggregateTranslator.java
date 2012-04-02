8a7bb175b6e6f400e38e58e946958c35372ee4db
mrg <mgentry@masslight.net>
2012-04-02 16:05:55 -0400

    Added distinct restriction and updated JavaDocts.

--- FetchingObjects/Aggregates/src/main/java/cbe/fetching/utilities/AggregateTranslator.java
+++ FetchingObjects/Aggregates/src/main/java/cbe/fetching/utilities/AggregateTranslator.java
@@ -3,8 +3,8 @@ package cbe.fetching.utilities;
 import org.apache.cayenne.access.trans.SelectTranslator;
 
 /**
- * Min() translator utility which extracts the SQL for a Cayenne-based query and
- * makes a SELECT MIN query from it.
+ * Aggregate translator utility which extracts the SQL for a Cayenne-based query and
+ * makes a custom SELECT query from it.
  *
  * @author mrg
  */
@@ -12,38 +12,62 @@ public class AggregateTranslator extends SelectTranslator
 {
     private final String function;
     private final String parameter;
+    private final boolean distinct;
 
     /**
-     * Specialized translator, which produces a SELECT MIN(column) from the
+     * Aggregate translator which produces a SELECT function(parameter) from a
      * Cayenne Query, such as SELECT MIN(PRICE).
      *
-     * @param column
+     * @param function The aggregate function to use.
+     * @param parameter
      *            A valid database column that can go between the parenthesis of
      *            a SELECT MIN(). Columns referenced must match the DB schema,
      *            not the Cayenne attribute.
+     * @param distinct Controls if DISTINCT is added to the parameter.
      */
-    public AggregateTranslator(String function, String parameter)
+    public AggregateTranslator(String function, String parameter, boolean distinct)
     {
-        this.function = function;
+        this.function  = function;
         this.parameter = parameter;
+        this.distinct  = distinct;
     }
 
+    /**
+     * @return The aggregate function used for this translator.
+     */
     public String getFunction()
     {
         return function;
     }
 
+    /**
+     * @return The aggregate parameter (usually a database column) used for this
+     *         translator.
+     */
     public String getParameter()
     {
         return parameter;
     }
 
+    /**
+     * @return If distinct is added to the aggregate function used for this
+     *         translator.
+     */
+    public boolean isDistinct()
+    {
+        return this.distinct;
+    }
+
+    /**
+     * Constructs the SQL for this translator.
+     *
+     * @see org.apache.cayenne.access.trans.SelectTranslator#createSqlString()
+     */
     @Override
     public String createSqlString() throws Exception
     {
         String sql = super.createSqlString();
 
-        return "SELECT " + function + "(" + parameter + ")"
-                + sql.substring(sql.indexOf(" FROM "));
+        return "SELECT " + function + "(" + (distinct ? "DISTINCT " : "") + parameter + ")" + sql.substring(sql.indexOf(" FROM "));
     }
 }
