cfa68965f247d796e12bea6f15b60431616f19b1
mrg <mgentry@masslight.net>
2012-04-02 16:09:48 -0400

    Added additional examples showing how to qualify the query.

--- FetchingObjects/Aggregates/src/main/java/cbe/fetching/Aggregates.java
+++ FetchingObjects/Aggregates/src/main/java/cbe/fetching/Aggregates.java
@@ -3,6 +3,7 @@ package cbe.fetching;
 import java.math.BigDecimal;
 
 import org.apache.cayenne.access.DataContext;
+import org.apache.cayenne.exp.ExpressionFactory;
 import org.apache.cayenne.query.SelectQuery;
 
 import cbe.fetching.model.Book;
@@ -27,6 +28,7 @@ public class Aggregates
         // Create a Query for Book records.
         SelectQuery query = new SelectQuery(Book.class);
 
+        // Run the aggregate queries.
         BigDecimal min = AggregatesUtil.min(dataContext, query, Book.PRICE_PROPERTY);
         BigDecimal max = AggregatesUtil.max(dataContext, query, Book.PRICE_PROPERTY);
         BigDecimal sum = AggregatesUtil.sum(dataContext, query, Book.PRICE_PROPERTY);
@@ -39,5 +41,22 @@ public class Aggregates
         System.out.println("Sum of Book Prices: " + sum);
         System.out.println("Average Book Price: " + avg);
         System.out.println("Number of Books: " + count);
+
+        // Add a qualifier for Author names starting with "J".
+        query.setQualifier(ExpressionFactory.likeIgnoreCaseExp(Book.AUTHOR_PROPERTY, "J%"));
+
+        // Run the aggregate queries.
+        min = AggregatesUtil.min(dataContext, query, Book.PRICE_PROPERTY);
+        max = AggregatesUtil.max(dataContext, query, Book.PRICE_PROPERTY);
+        sum = AggregatesUtil.sum(dataContext, query, Book.PRICE_PROPERTY);
+        avg = AggregatesUtil.avg(dataContext, query, Book.PRICE_PROPERTY);
+        count = AggregatesUtil.count(dataContext, query);
+
+        // Print the results.
+        System.out.println("Minimum 'J' Author Book Price: " + min);
+        System.out.println("Maximum 'J' Author Book Price: " + max);
+        System.out.println("Sum of 'J' Author Book Prices: " + sum);
+        System.out.println("Average 'J' Author Book Price: " + avg);
+        System.out.println("Number of 'J' Author Books: " + count);
     }
 }
