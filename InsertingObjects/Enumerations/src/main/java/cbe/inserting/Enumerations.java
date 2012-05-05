5d1fb9f1a53ed01f37036d0ad80c3ec850aed3fc
mrg <blacknext@gmail.com>
2012-05-05 14:59:37 -0400

    Moved to a main program with constructor and inlined person creating (no auxiliary input file).

--- InsertingObjects/Enumerations/src/main/java/cbe/inserting/Enumerations.java
+++ InsertingObjects/Enumerations/src/main/java/cbe/inserting/Enumerations.java
@@ -4,35 +4,38 @@ import org.apache.cayenne.access.DataContext;
 
 import cbe.inserting.constants.RoleType;
 import cbe.inserting.model.Person;
-import cbe.inserting.utilities.Populator;
 
 /**
  * Cayenne By Example - https://github.com/mrg/cbe
- * 
+ *
  * This example builds upon BasicInserts3.
- * 
+ *
  * It inserts many Person objects into the database within a single
- * commit/transaction.
- * 
- * The data is read from 'People.txt' under resources (loaded by Populator) and
- * a Role is added in order to illustrate mapping Java enumerations to database
- * values. Cayenne uses the RoleType enumeration to handle the translation
- * values (look at the Java Type in the modeler).
- * 
+ * commit/transaction.  A Role is added in order to illustrate mapping
+ * Java enumerations to database values. Cayenne uses the RoleType
+ * enumeration to handle the translation values (look at the Java Type
+ * in Cayenne Modeler).
+ *
  * @author mrg
  */
 public class Enumerations
 {
-    public static void main(String[] arguments)
+    DataContext dataContext = null;
+
+    public Enumerations()
     {
         // Create a new DataContext. This will also initialize the Cayenne
         // Framework.
-        DataContext dataContext = DataContext.createDataContext();
+        dataContext = DataContext.createDataContext();
 
-        // Loop over all the names in our resources file and create Persons
-        // for each of them.
-        for (String[] personFields : Populator.getPeople())
-            createPerson(dataContext, personFields);
+        // Create People records (in the DataContext).
+        createPerson("System", "Administrator", "admin@example.com", "ADMIN");
+        createPerson("Aaron", "Caldwell", "acaldwell@example.com", "AUTHOR");
+        createPerson("Heidi", "Freeman", "hfreeman@example.com", "EDITOR");
+        createPerson("Marcus", "Kerr", "mkerr@example.com", "MODERATOR");
+        createPerson("Rose", "Newton", "rnewton@example.com", "EDITOR");
+        createPerson("Ulric", "Reeves", "ureeves@example.com", "NONE");
+        createPerson("Victoria", "Waters", "vwaters@example.com", "AUTHOR");
 
         // Commit the changes to the database.
         dataContext.commitChanges();
@@ -40,27 +43,30 @@ public class Enumerations
 
     /**
      * Helper method to create and initialize a person in a DataContext.
-     *
-     * @param dataContext The DataContext to register the person.
-     * @param fields The data fields from the People.txt file.
      */
-    private static void createPerson(DataContext dataContext, String[] fields)
+    private void createPerson(String firstName,
+                              String lastName,
+                              String emailAddress,
+                              String roleType)
     {
-    	// Extract field values.
-    	String firstName    = fields[Populator.PERSON_FIRST_NAME];
-    	String lastName     = fields[Populator.PERSON_LAST_NAME];
-    	String emailAddress = fields[Populator.PERSON_EMAIL_ADDRESS];
-    	String roleType     = fields[Populator.PERSON_ROLE_TYPE];
-
         // Create a new Person object tracked by the DataContext.
     	Person person = dataContext.newObject(Person.class);
 
-        // Set values for the new person. Defaults the password to the e-mail
-        // address with "123" appended.
+        // Set values for the new person.
         person.setFirstName(firstName);
         person.setLastName(lastName);
         person.setEmailAddress(emailAddress);
+
+        // Default the password to the e-mail address with "123" appended.
         person.setPassword(emailAddress + "123");
+
+        // Convert the role (a String) to a RoleType.
         person.setRole(RoleType.valueOf(roleType));
+    }
+
+
+    public static void main(String[] arguments)
+    {
+        new Enumerations();
     }
 }
