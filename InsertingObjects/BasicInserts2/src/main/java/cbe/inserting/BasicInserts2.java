4dfa2c94c17629ca558a9c6b5003261db0a3d55b
mrg <blacknext@gmail.com>
2012-05-05 14:39:13 -0400

    Moved to a main program with constructor and inlined person creating (no auxiliary input file).

--- InsertingObjects/BasicInserts2/src/main/java/cbe/inserting/BasicInserts2.java
+++ InsertingObjects/BasicInserts2/src/main/java/cbe/inserting/BasicInserts2.java
@@ -3,33 +3,34 @@ package cbe.inserting;
 import org.apache.cayenne.access.DataContext;
 
 import cbe.inserting.model.Person;
-import cbe.inserting.utilities.Populator;
 
 /**
  * Cayenne By Example - https://github.com/mrg/cbe
- * 
+ *
  * This example builds upon BasicInserts1.
- * 
+ *
  * It inserts many Person objects into the database within a single
  * commit/transaction.
- * 
- * The data is read from 'People.txt' under resources (loaded by Populator)
- * and adds an email address field.
- * 
+ *
  * @author mrg
  */
 public class BasicInserts2
 {
-    public static void main(String[] arguments)
-    {
-        // Create a new DataContext. This will also initialize the Cayenne
-        // Framework.
-        DataContext dataContext = DataContext.createDataContext();
+    DataContext dataContext = null;
 
-        // Loop over all the names in our resources file and create Persons
-        // for each of them.
-        for (String[] personFields : Populator.getPeople())
-            createPerson(dataContext, personFields);
+    public BasicInserts2()
+    {
+         // Create a new DataContext. This will also initialize the Cayenne
+         // Framework.
+         dataContext = DataContext.createDataContext();
+
+         // Create People records (in the DataContext).
+         createPerson("Aaron", "Caldwell", "acaldwell@example.com");
+         createPerson("Heidi", "Freeman", "hfreeman@example.com");
+         createPerson("Marcus", "Kerr", "mkerr@example.com");
+         createPerson("Rose", "Newton", "rnewton@example.com");
+         createPerson("Ulric", "Reeves", "ureeves@example.com");
+         createPerson("Victoria", "Waters", "vwaters@example.com");
 
         // Commit the changes to the database.
         dataContext.commitChanges();
@@ -37,17 +38,9 @@ public class BasicInserts2
 
     /**
      * Helper method to create and initialize a person in a DataContext.
-     *
-     * @param dataContext The DataContext to register the person.
-     * @param fields The data fields from the People.txt file.
      */
-    private static void createPerson(DataContext dataContext, String[] fields)
+    private void createPerson(String firstName, String lastName, String emailAddress)
     {
-    	// Extract field values.
-    	String firstName    = fields[Populator.PERSON_FIRST_NAME];
-    	String lastName     = fields[Populator.PERSON_LAST_NAME];
-    	String emailAddress = fields[Populator.PERSON_EMAIL_ADDRESS];
-
         // Create a new Person object tracked by the DataContext.
     	Person person = dataContext.newObject(Person.class);
 
@@ -55,5 +48,11 @@ public class BasicInserts2
         person.setFirstName(firstName);
         person.setLastName(lastName);
         person.setEmailAddress(emailAddress);
+    }
+
+
+    public static void main(String[] arguments)
+    {
+        new BasicInserts2();
     }
 }
