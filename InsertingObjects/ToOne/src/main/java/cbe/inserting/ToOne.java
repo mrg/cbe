c686097db01623b2f3d8387c0b2f9469e2f4fd1b
mrg <blacknext@gmail.com>
2012-05-12 09:36:41 -0400

    Updated to use Person instead of User and simplified example logic.

--- InsertingObjects/ToOne/src/main/java/cbe/inserting/ToOne.java
+++ InsertingObjects/ToOne/src/main/java/cbe/inserting/ToOne.java
@@ -1,13 +1,9 @@
 package cbe.inserting;
 
-import java.util.Random;
-
 import org.apache.cayenne.access.DataContext;
 
-import cbe.inserting.constants.RoleType;
 import cbe.inserting.model.Address;
 import cbe.inserting.model.Person;
-import cbe.inserting.utilities.Populator;
 
 /**
  * Cayenne By Example - https://github.com/mrg/cbe
@@ -28,116 +24,36 @@ public class ToOne
         // Framework.
         dataContext = DataContext.createDataContext();
 
-    }
-
-    public static void main(String[] arguments)
-    {
-
-
-
-        // Create a new Person object tracked by the DataContext.
-        Person admin = dataContext.newObject(Person.class);
-
-        // Set values for the new person. In this case, we are initializing
-        // an administrator.
-        admin.setFirstName("System");
-        admin.setLastName("Administrator");
-        admin.setUsername("admin");
-        admin.setPassword("admin123");
-        admin.setEnabled(true);
-        admin.setRole(RoleType.ADMIN);
-
-        // Loop over all the names in our resources file and create persons
-        // for each of them.
-        for (String firstName : Populator.getFirstNames())
-            for (String lastName : Populator.getLastNames())
-                createPerson(dataContext, firstName, lastName);
-
-        // Commit the changes to the database.  This includes Person
-        // and Address records.
-        dataContext.commitChanges();
-    }
-
-    /**
-     * Helper method to create and initialize a person in a DataContext.
-     *
-     * @param dataContext The DataContext to register the person.
-     * @param firstName The person's first name.
-     * @param lastName The person's last name.
-     */
-    private static void createPerson(DataContext dataContext, String firstName, String lastName)
-    {
         // Create a new Person object tracked by the DataContext.
         Person person = dataContext.newObject(Person.class);
 
-        // Set values for the new person. Defaults the password to the username
-        // with "123" appended.
-        person.setFirstName(firstName);
-        person.setLastName(lastName);
-        person.setUsername((firstName.substring(0, 1) + lastName).toLowerCase());
-        person.setPassword(person.getUsername() + "123");
+        // Set values for the new person. In this case, we are initializing
+        // an administrator.
+        person.setFirstName("System");
+        person.setLastName("Administrator");
+        person.setUsername("admin");
 
-        // Don't enable accounts whose last name starts with an "A".
-        if (lastName.startsWith("A"))
-            person.setEnabled(false);
-        else
-            person.setEnabled(true);
-
-        // Accounts whose last name starts with a "B" are moderators.
-        if (lastName.startsWith("B"))
-            person.setRole(RoleType.MODERATOR);
-        else
-            person.setRole(RoleType.CUSTOMER);
-
-        // Create an address for this person.
-        createAddress(person);
-    }
-
-    /**
-     * Creates an address for a person.
-     *
-     * @param person The person to create the address.
-     */
-    private static void createAddress(Person person)
-    {
-        // Extract the DataContext from the person.  We are creating a
-        // relationship and Cayenne requires that related objects reside
-        // in the same DataContext, so be sure to use the same one.
-        DataContext dataContext = (DataContext) person.getObjectContext();
-
-        // Create the new address in the person's DataContext.
+        // Create the address for the person.
         Address address = dataContext.newObject(Address.class);
 
         // Set the address attributes.
         address.setCity("Falls Church");
         address.setState("VA");
-        address.setStreet(getRandomStreet());
+        address.setStreet("W Broad Street");
         address.setZip("22046");
 
         // Assign the address to the person.  Cayenne will figure out how
         // to map the relationship based upon the model and also create
         // the primary keys and foreign keys.
         person.setAddress(address);
+
+        // Commit the changes to the database (both the Person
+        // and Address records).
+        dataContext.commitChanges();
     }
 
-    /**
-     * @return A made-up street address.
-     */
-    private static String getRandomStreet()
+    public static void main(String[] arguments)
     {
-        int    number = new Random().nextInt(1000) + 1;
-        String street;
-
-        switch (new Random().nextInt(5))
-        {
-            case 0: street = "W Broad Street"; break;
-            case 1: street = "N Washington Street"; break;
-            case 2: street = "N West Street"; break;
-            case 3: street = "Great Falls Street"; break;
-            case 4: street = "Annandale Road"; break;
-            default: street = "Lincoln Avenue";
-        }
-
-        return number + " " + street;
+        new ToOne();
     }
 }
