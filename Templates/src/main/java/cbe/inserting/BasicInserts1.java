2f20d173dce3eac03213f1e816cd53bbf23cc8f0
mrg <mgentry@masslight.net>
2013-01-29 17:37:11 -0500

    Adding example with custom templates.

---
+++ Templates/src/main/java/cbe/inserting/BasicInserts1.java
@@ -0,0 +1,48 @@
+package cbe.inserting;
+
+import org.apache.cayenne.access.DataContext;
+
+import cbe.inserting.model.Person;
+
+/**
+ * Cayenne By Example Source Code:
+ *   https://github.com/mrg/cbe
+ *   https://github.com/mrg/cbe/tree/master/Templates
+ *
+ * Cayenne By Example Documentation:
+ *   http://mrg.github.com/cbe/
+ *   http://mrg.github.com/cbe/custom-templates.html
+ *
+ * This example is a near-clone of the BasicInserts (#1) example, but
+ * utilizes custom templates (subclass.vm and superclass.vm) to generate
+ * the Cayenne mapping classes (Person.java and _Person.java).
+ *
+ * @author mrg
+ */
+public class BasicInserts1
+{
+    DataContext dataContext = null;
+
+    public BasicInserts1()
+    {
+        // Create a new DataContext. This will also initialize the Cayenne
+        // Framework.
+        dataContext = DataContext.createDataContext();
+
+        // Create a new Person object tracked by the DataContext.
+        Person person = dataContext.newObject(Person.class);
+
+        // Set values for the new person.
+        person.setFirstName("System");
+        person.setLastName("Administrator");
+        person.setEmailAddress("admin@example.com");
+
+        // Commit the changes to the database.
+        dataContext.commitChanges();
+    }
+
+    public static void main(String[] arguments)
+    {
+        new BasicInserts1();
+    }
+}
---
+++ Templates/src/main/java/cbe/inserting/model/Person.java
@@ -0,0 +1,30 @@
+package cbe.inserting.model;
+
+import cbe.inserting.model.auto._Person;
+
+/**
+ *
+ */
+public class Person extends _Person
+{
+    private static final long serialVersionUID = 1L;
+
+    /***********************************************************************/
+    /*** Custom Accessors                                                ***/
+    /***********************************************************************/
+
+    // Put is*/get*/set* methods here.
+
+    /***********************************************************************/
+    /*** Business Logic                                                  ***/
+    /***********************************************************************/
+
+    // Put non-accessor methods here.
+
+    /***********************************************************************/
+    /*** Utilities                                                       ***/
+    /***********************************************************************/
+
+    // Put utility-type methods here, which are typically static, such as
+    // fetch* type methods, which fetch Cayenne objects from the database.
+}
