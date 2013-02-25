dee532c35d51c655f44ceb8e3deb3860121ea0de
mrg <mgentry@masslight.net>
2013-02-25 13:20:11 -0500

    Added startup initialization of Flyway.

--- WebApplications/src/main/java/cbe/app/services/AppModule.java
+++ WebApplications/src/main/java/cbe/app/services/AppModule.java
@@ -1,12 +1,33 @@
 package cbe.app.services;
 
+import javax.sql.DataSource;
+
+import org.apache.cayenne.access.DataContext;
 import org.apache.commons.lang.BooleanUtils;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
+import org.apache.tapestry5.ioc.annotations.Startup;
+
+import com.googlecode.flyway.core.Flyway;
+import com.googlecode.flyway.core.api.MigrationVersion;
 
 public class AppModule
 {
+    @Startup
+    public void initializeDatabase()
+    {
+        DataContext context    = DataContext.createDataContext();
+        DataSource  dataSource = context.getParentDataDomain().getNode("CBE").getDataSource(); //.getConnection();
+        Flyway      flyway     = new Flyway();
+
+        flyway.setDataSource(dataSource);
+        flyway.setInitialVersion("1.0");
+        flyway.setInitOnMigrate(true);
+        flyway.setTarget(MigrationVersion.LATEST);
+        flyway.migrate();
+    }
+
     public static void bind(ServiceBinder binder)
     {
     }
