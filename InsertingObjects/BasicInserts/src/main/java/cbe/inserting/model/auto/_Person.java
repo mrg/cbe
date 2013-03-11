7db92757ea8349a430eba8387a96b837a7859715
mrg <mgentry@masslight.net>
2013-03-11 15:55:44 -0400

    Regenerated _Person after uppercasing database columns.

--- InsertingObjects/BasicInserts/src/main/java/cbe/inserting/model/auto/_Person.java
+++ InsertingObjects/BasicInserts/src/main/java/cbe/inserting/model/auto/_Person.java
@@ -15,7 +15,7 @@ public abstract class _Person extends CayenneDataObject {
     public static final String LAST_NAME_PROPERTY = "lastName";
     public static final String PASSWORD_PROPERTY = "password";
 
-    public static final String ID_PK_COLUMN = "id";
+    public static final String ID_PK_COLUMN = "ID";
 
     public void setEmailAddress(String emailAddress) {
         writeProperty("emailAddress", emailAddress);
